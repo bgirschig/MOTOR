03e498f18754423470391518daf4e962b16f2537
bastien girschig <bastien.girschig@gmail.com>
2018-09-29 02:26:29 +0200

    Massive refactor for error handling, versatility (the whole scraping + render request structure is defined in one json file)

---
+++ html_parser.py
@@ -0,0 +1,130 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+from urlparse import urljoin
+import logging
+
+ACCEPT_STATUS_CODES = [200, 201, 202]
+
+def scrapeUrl(url, scraper_def):
+    """parses a product page for useful information about that product
+    
+    Arguments:
+        url {string} -- the url of the page to be parsed
+    """
+    try:
+        with open(scraper_def) as f:
+            scraper_def = json.load(f)
+
+        resp = requests.get(url)
+
+        if resp.status_code not in ACCEPT_STATUS_CODES:
+            raise Exception('failed request: [{}] {}'.format(
+                resp.status_code, resp.content))
+
+        pageContent = resp.text
+        tree = BeautifulSoup(pageContent, 'html.parser') # TODO: check if lxml *parser* works and is faster
+        scraped = scrape(tree, scraper_def)
+
+        return scraped
+    except Exception as err:
+        errorObj = {
+            'error': str(err),
+            'baseUrl': url,
+            'status_code': resp.status_code,
+        }
+        logging.warning(errorObj)
+        return errorObj
+        
+
+def scrape(tree, obj):
+    if type(obj) == str or type(obj) == unicode:
+        return obj
+    elif type(obj) == list:
+        return [scrape(tree, item) for item in obj]
+    elif type(obj) == dict:
+        for key in obj:
+            if key.startswith('scrape_'):
+                newKey = key.replace('scrape_', '')
+                obj[newKey] = scrapeItem(tree, obj[key])
+                del obj[key]
+            else:
+                obj[key] = scrape(tree, obj[key])
+        return obj
+    else:
+        raise Exception('unexpected value type: '+type())
+
+def scrapeItem(tree, selector):
+    # handle selectors that don't have modifiers. Defaults to 'text,first'
+    if ' & ' not in selector: selector += ' & text,first'
+
+    # retrieve css selector and modifiers
+    cssselector, modifiers = selector.split(' & ')
+    cssselector = cssselector.strip()
+    modifiers = modifiers.split(',')
+    modifiers = [modifier.strip() for modifier in modifiers]
+
+    # get elements matching the css selector
+    output = tree.select(cssselector)
+    if len(output) == 0 and ('required' in modifiers or
+                             'first' in modifiers or
+                             'last' in modifiers):
+        raise KeyError('missing required data for selector {}'.format(selector))
+
+    # apply modifiers (what to get from the html node)
+    if 'text' in modifiers:
+        output = [item.string for item in output]
+    elif 'src' in modifiers:
+        output = [item['src'] for item in output]
+
+    # strip the text of whitespaces and newlines
+    output = [item.strip() for item in output]
+
+    # apply modifiers (select item in the returned list)
+    if 'first' in modifiers:
+        output = output[0]
+    elif 'last' in modifiers:
+        output = output[-1]
+
+    return output
+
+if __name__ == '__main__':
+    # scraper_def defines what data we want to extract, and how it should be
+    # formatted. An element whose key is prefixed with 'scrape_' should be a
+    # selector. It will be replaced by the scraped version of itself. The prefix
+    # will be removed from the key.
+    # This particular def mathes the pattern for a MOTOR render request (without
+    # the dynamic elements, like request id, requester, etc...)
+    # A selector is a css selector with a list of comma separated flags that
+    # define more details on how to handle that item:
+    #   - first, last, etc...: only keep the first, last, etc... item from the
+    #   css selector. This is useful when you only want one element. otherwise,
+    #   you get a list of items
+    #   - src: get the src of the element
+    #   - text: get the text content of the element
+    #   - required: throw an error if the item is not found
+    scraper_def = {
+        "template": "chanel_test",
+        "compName": "main",
+        "resources": [
+            {"target": "main_image.jpg", "scrape_src": ".product-details__media img & src,first,required"},
+            {"target": "variants#.jpg", "scrape_src": ".tint-image & src"},
+            {"target": "data.json", "data": {
+                'scrape_price': '.product-details__price & text,first,required',
+                'scrape_title': '.product-details__title & text,first,required',
+                'scrape_description': '.product-details__description & text,first',
+                'scrape_reference': '.product-details__reference & text,first',
+                'scrape_flags': '.product-details__flags .flag & text',
+            }}
+        ],
+        "encoders": [
+            {"presetName": "smol_vid", "filename": "video_a"}
+        ]
+    }
+
+    # a few urls to try from
+    # url = 'https://www.chanel.com/us/makeup/p/168820/joues-contraste-powder-blush/'
+    # url = 'https://www.chanel.com/us/makeup/p/170816/vitalumiere-aqua-ultra-light-skin-perfecting-sunscreen-makeup-spf-15/'
+    url = 'http://www.google.com'
+
+    print json.dumps(parsePage(url, parser_def), indent=3)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
 import mailParser
-import productParser
-from MotorRequest import MotorRequest
+import html_parser
 
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.api import mail
@@ -10,9 +9,18 @@ import webapp2
 import jinja2
 import os
 from os import path
+import json
+from time import time
+import hashlib
+from random import randrange
+from google.appengine.api import urlfetch
+
 
 SELF_EMAIL = "render@kairos-motor.appspotmail.com"
-TEMPLATES_PATH = path.join(path.dirname(__file__), 'templates')
+ACCEPT_STATUS_CODES = [200, 201, 202]
+RENDERER_API_URL = 'http://40.89.131.172:8081/render'
+# RENDERER_API_URL = 'http://localhost:8081/render'
+TEMPLATES_PATH = path.join(path.dirname(__file__), 'scrapers')
 
 # TODO: use cloud endpoints for managing user limits, monitoring, etc...
 # TODO: move render nodes to gcloud compute engine
@@ -33,26 +41,69 @@ class MailRequestHandler(InboundMailHandler):
             'Received a message:\n' + mailParser.stringify(mail_message))
 
         # Retrieve data from mail text
-        data = mailParser.parse(mail_message)
+        maildata = mailParser.parse(mail_message)
         
         # Scrape found 'requests' for render infos
-        items = [productParser.parseItem(item) for item in data['items']]
-        
-        # create request
-        requests = [MotorRequest(mail_message.sender, mail_message.sender, item) for item in items]
+        requests = []
+        for mail_request_item in maildata['requests']:
+            request_datas = html_parser.scrapeUrl(
+                mail_request_item['url'], './scrapers/chanel_makeup.json')
+            
+            # determine request id
+            string_req = json.dumps(request_datas) + str(randrange(0, 10**14))
+            request_datas['id'] = hashlib.md5(string_req).hexdigest()
+            
+            # append other request informations
+            request_datas["clientID"] = mail_message.sender
+            request_datas["requesterID"] = mail_message.sender
+            request_datas["timestamp"] = time()
+
+            requests.append(request_datas)
 
         # Send recap email: parsed info, parsing errors, etc...
         send_recap_mail(requests, mail_message.sender)
         
         for request in requests:
-            request.send()
+            send_request(request)
+
         logging.info('[main handler] done')
+
+def send_request(request_data):
+    try:
+        logging.info({'tag': 'render-request', 'message': 'sending request',
+            'request_data': request_data})
+
+        result = urlfetch.fetch(
+            url=RENDERER_API_URL,
+            method=urlfetch.POST,
+            payload=json.dumps(request_data),
+            headers={
+                'Content-Type': 'application/json'
+            }
+        )
+        if result.status_code not in ACCEPT_STATUS_CODES:
+            raise Exception('failed request: [{}] {}'.format(
+                result.status_code, result.content))
+        
+        logging.info({ 'tag': 'render-request', 'message': 'sent request',
+            'request_id': request_data['id']})
+        
+    except Exception as err:
+        logging.error({
+            'tag': 'render-request',
+            'type': type(err).__name__,
+            'message': err.message,
+            'request_id': request_data['id'],
+            'request_data': request_data,
+        })
 
 def send_recap_mail(requests, address):
     logging.info('send_recap_mail')
-    html_template = jinja.get_template('request_confirm_mail.html')
+
+    html_template = jinja.get_template('chanel_makeup_mail.html')
     mail_content_html = html_template.render({'requests': requests})
-    text_template = jinja.get_template('request_confirm_mail.txt')
+    
+    text_template = jinja.get_template('chanel_makeup_mail.txt')
     mail_content_text = text_template.render({'requests': requests})
 
     message = mail.EmailMessage(
