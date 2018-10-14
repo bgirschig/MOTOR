494171f2f2116561228f65022f2d3e8714ee5f4c
bastien girschig <bastien.girschig@gmail.com>
2018-10-14 20:16:08 +0200

    remove deprecated "render_service"

--- render_service/appengine_config.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import vendor
-vendor.add('lib')
-
-# import requests_toolbelt.adapters.appengine
-
-# # Use the App Engine Requests adapter. This makes sure that Requests uses
-# # URLFetch.
-# requests_toolbelt.adapters.appengine.monkeypatch()
