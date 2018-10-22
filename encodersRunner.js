a99f781906fd5893e2103b30f3e5dafb81eab1e7
bastien girschig <bastien.girschig@gmail.com>
2018-10-22 07:35:00 +0200

    Move from adobe media encoder to ffmpeg for encoding

--- encodersRunner.js
+++ encodersRunner.js
@@ -1,42 +1,48 @@
-const ame_webservice = require('./third_party/adobe-media-encoder-webservice');
 const path = require("path");
 const config = require("./config");
-
-var ame = new ame_webservice.AdobeMediaEncoder(config.media_encoder);
+const spawn = require('child_process').spawn;
 
 function runEncoders (source, encoders, logger) {
     logger.info('encoding')
-    return ame.start()
-    .then(()=>{
-        let jobPromises = [];
-        let dirname = path.dirname(source);
+    
+    let promises = [];
+    let dirname = path.dirname(source);
 
-        // for each encoder, enqueue a job, and add its promise to the list
-        for (let encoder of encoders) {
-            jobPromises.push(new Promise((resolve, reject)=>{
-                let presetPath = path.join(
-                    config.presets_dir, encoder.presetName + '.epr');
-                let destPath = path.join(dirname, encoder.filename);
+    // for each encoder, enqueue a job, and add its promise to the list
+    for (let encoder of encoders) {
+        console.log(encoder);
+        argString = `-y -i ${source} ${encoder}`
+        promises.push(ffmpeg(
+            path.resolve(source),
+            argString,
+            {cwd:dirname, logger: logger}
+        ));
+    }
 
-                let job = ame.enqueueJob({
-                    sourceFilePath: path.resolve(source),
-                    destinationPath: path.resolve(destPath),
-                    sourcePresetPath: path.resolve(presetPath),
-                });
+    // Wait for all jobs to complete.
+    return Promise.all(promises);
+}
 
-                job.on('progress', ()=>{
-                    logger.info({}, `${job.statusText} ${job.progress} ${job.statusDetail}`)
-                });
+async function ffmpeg(source, argString, options={}) {
+    options.logger.info(`spawn process: ffmpeg ${argString}`);
 
-                job.on('ended', ()=>{
-                    logger.info({}, `${job.status}, ${job.lastStatusResponse}`)
-                    resolve(job.status, job.lastStatusResponse);
-                });
-            }));
-        }
-
-        // Wait for all jobs to complete.
-        return Promise.all(jobPromises);
+    let args = argString.split(' ');
+    var ffmpeg = spawn('ffmpeg', args, options);
+    
+    ffmpeg.on('error', (err) => {
+        options.logger.error('ffmpeg error:'+err, String(err));
+    });
+    ffmpeg.stdout.on('data', data => {
+        options.logger.info('ffmpeg stdout: ' + data.toString().trim());
+    })
+    ffmpeg.stderr.on('data', (data) => {
+        options.logger.error('ffmpeg stderr:', data.toString());
+    });
+    return new Promise((resolve, reject)=>{
+        ffmpeg.on('close', function (code) {
+            if (code == 0) resolve();
+            else reject('ffmpeg command failed');
+        });
     })
 }
 
