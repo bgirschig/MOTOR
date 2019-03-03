59853eb6637d9cb11fbed84c4e1f8593fc0ebe93
bastien girschig <bastien.girschig@gmail.com>
2019-03-04 00:36:27 +0100

    image conversion from motor forms

--- forms_service/formResponseHandler.py
+++ forms_service/formResponseHandler.py
@@ -12,6 +12,8 @@ from common import file_utils
 from common import utils
 from common.task_queue_client import Queue
 from google.appengine.api import users
+from io import BytesIO
+from google.appengine.api import images
 
 # config
 MAX_FILE_SIZE = 1 * utils.MB
@@ -62,21 +64,83 @@ def extractData(obj, fields, form_definition):
       else:
         obj[key] = extractData(obj[key], fields, form_definition)
     return obj
+  elif type(obj) == type(None):
+    return obj
   else:
-      raise Exception('unexpected value type: '+type())
+      raise Exception('unexpected value type: '+str(type(obj)))
 
 def parseItem(field_name, fields, options):
   output = []
   values = fields.getall(field_name)
   for value in values:
+    # files are uploaded and their urls are used in the request
     if file_utils.isFile(value):
       file_utils.checkFile_size(value.file, MAX_FILE_SIZE)
+      # image converters
+      if "convert_jpg" in options:
+        value = convertFormFile(value, 'image/jpeg')
+      elif "convert_png" in options:
+        value = convertFormFile(value, 'image/png')
       output.append(upload_file(value.file, value.type))
+   
+    # If there is an "_other" option in a choices list, the form renderer adds
+    # an input field to specify what that "other" is, with the name:
+    # <field_name><other_suffix>, eg. 'language_other'
     elif(value == "other" and field_name+other_suffix in fields):
       output.append(fields[field_name+other_suffix])
+    
+    # for 'normal' fields, simply add them to the output
     else:
       output.append(fields[field_name])
   
   if("multi" not in options):
-    output = output[0]
+    output = output[0] if len(output)>0 else None
   return output
+
+"""
+Converts the given File_field to another format.
+Mime 'kinds' should match (image to image, text to text, etc...)
+If the input already has the target mime type, the file object is returned.
+"""
+def convertFormFile(formValue, target_mime):
+  output = File_field(formValue.file, formValue.type)
+
+  # Mime types match. No need to convert
+  if formValue.type == target_mime:
+    return output
+  
+  data_kind, data_type = formValue.type.split("/")
+  target_kind, target_type = target_mime.split("/")
+  
+  # convert images
+  if data_kind == "image" and target_kind == "image":
+    img = images.Image(formValue.file.getvalue())
+    
+    # App engine's image API requires at least one transform. This is a no-op
+    # that gets around this limitation (we only need to convert the image
+    # format)
+    img.rotate(360)
+
+    if target_type == "jpeg":
+      output.file = BytesIO(img.execute_transforms(output_encoding=images.JPEG))
+      output.type = 'image/jpg'
+    elif target_type == "png":
+      output.file = BytesIO(img.execute_transforms(output_encoding=images.PNG))
+      output.type = 'image/png'
+    elif target_type == "webp":
+      output.file = BytesIO(img.execute_transforms(output_encoding=images.WEBP))
+      output.type = 'image/webp'
+    else:
+      raise ValueError("No converter defined to {} image".format(target_type))
+
+  else:
+    raise TypeError("No converter defined from types {} to {}".format(
+      formValue.type,
+      target_mime,
+    ))
+  return output
+
+class File_field():
+  def __init__(self, file_, mime_type):
+    self.file = file_
+    self.type = mime_type
